3d69aa694fee5b811b38ca679da30fdd84c9d6c7
ConnorBStill <connor@lamarsoftware.io>
2025-01-15 16:43:22 -0800

    Remove console log

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,6 @@
 import Link from 'next/link';
 
 export default function HomePage() {
-  console.log('This doesnt need to be here')
   return (
     <main>
       <div className="container flex flex-col items-center justify-center gap-12 px-4 py-16">
