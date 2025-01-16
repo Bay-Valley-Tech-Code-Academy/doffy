94239d30163f7a2d27cc7032e647cce9e67a7506
ConnorBStill <connor@lamarsoftware.io>
2025-01-15 16:41:08 -0800

    Fix login form bug

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,11 @@
 import Link from 'next/link';
 
 export default function HomePage() {
+  console.log('This doesnt need to be here')
   return (
     <main>
       <div className="container flex flex-col items-center justify-center gap-12 px-4 py-16">
-        <h5>Job listing page</h5>
+        <h5>Job listing page!</h5>
       </div>
     </main>
   );
