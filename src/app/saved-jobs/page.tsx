56d3bc630240dae01c8014b1da679fd830579daf
CMBVT2023 <cristianomirandabvt2023@gmail.com>
2025-01-17 11:45:08 -0800

    Made the buttons interactive and created temporary data to display on the page

--- src/app/saved-jobs/page.tsx
+++ src/app/saved-jobs/page.tsx
@@ -1,21 +1,41 @@
+"use client"
 import Link from 'next/link';
-import { Button } from '../../components/ui/button'
 import SavedJob from './SavedJob'
+import ButtonWithBadge from './ButtonWithBadge';
+import { useState } from 'react';
+import { Job, savedJobsArr, appliedJobsArr, interviewJobsArr, archivedJobsArr } from './placeHolderData';
 
 export default function SavedJobsPage() {
+  const [ jobListings, setJobListings ] = useState<Array<Job>>(savedJobsArr);
+
+  function assignCurrentTab(tab: string): void {
+      let jobsList: Array<Job>;
+      if (tab == 'Saved') {
+        jobsList = savedJobsArr;
+      } else if (tab == 'Applied') {
+        jobsList = appliedJobsArr
+      } else if (tab == "Interviews") {
+        jobsList = interviewJobsArr
+      } else {
+        jobsList = archivedJobsArr
+      }
+      setJobListings(jobsList)
+  }
+
   return (
-    <main>
-      <div className="w-screen flex flex-col justify-center items-center gap-12 px-4 py-16">
-        <h2>My Jobs</h2>
-        <div>
-          <Button>Saved</Button>
-          <Button>Applied</Button>
-          <Button>Interviews</Button>
-          <Button>Archived</Button>
+    <main className='flex h-full justify-center'>
+      <div className="container flex flex-col justify-center items-center gap-3 px-4 py-16">
+        <h2 className='text-4xl font-extrabold self-start'>My Jobs</h2>
+
+        <div className='container flex gap-2 border-b-black border border-transparent p-1'>
+          <ButtonWithBadge badgeText={savedJobsArr.length} onClick={() => assignCurrentTab('Saved')}>Saved</ButtonWithBadge>
+          <ButtonWithBadge badgeText={appliedJobsArr.length} onClick={() => assignCurrentTab('Applied')}>Applied</ButtonWithBadge>
+          <ButtonWithBadge badgeText={interviewJobsArr.length} onClick={() => assignCurrentTab('Interviews')}>Interviews</ButtonWithBadge>
+          <ButtonWithBadge badgeText={archivedJobsArr.length} onClick={() => assignCurrentTab('Archived')}>Archived</ButtonWithBadge>
         </div>
 
-        <div className='container'>
-            <SavedJob />
+        <div className='container h-auto overflow-auto'>
+            {jobListings.map(job => <SavedJob key={job.jobTitle} jobTitle={job.jobTitle} company={job.company} location={job.location} savedDate={job.savedDate} companyImage={job.companyImage}/>)}
         </div>
       </div>
     </main>
---
+++ src/app/saved-jobs/placeHolderData.ts
@@ -0,0 +1,98 @@
+export type Job = {
+    jobTitle: string;
+    company: string;
+    location: string;
+    savedDate: string;
+    companyImage?: string;
+}
+
+export const savedJobsArr: Array<Job> = [
+    {
+        jobTitle: "React Dev",
+        company: "Some Company",
+        location: "Modesto, CA",
+        savedDate: "Saved Today"
+    },
+    {
+        jobTitle: "Web Dev",
+        company: "WebDevelopment Inc",
+        location: "Riverbank, CA",
+        savedDate: "Saved Yesterday"
+    },
+]
+
+export const appliedJobsArr: Array<Job> = [
+    {
+        jobTitle: "NextJS Web Designed",
+        company: "NextJS Studio",
+        location: "Modesto, CA",
+        savedDate: "Saved Yesterday"
+    },
+    {
+        jobTitle: "NextJS Web Designed",
+        company: "NextJS Studio",
+        location: "Modesto, CA",
+        savedDate: "Saved Yesterday"
+    },
+    {
+        jobTitle: "NextJS Web Designed",
+        company: "NextJS Studio",
+        location: "Modesto, CA",
+        savedDate: "Saved Yesterday"
+    },
+    {
+        jobTitle: "NextJS Web Designed",
+        company: "NextJS Studio",
+        location: "Modesto, CA",
+        savedDate: "Saved Yesterday"
+    },
+    {
+        jobTitle: "NextJS Web Designed",
+        company: "NextJS Studio",
+        location: "Modesto, CA",
+        savedDate: "Saved Yesterday"
+    },
+    {
+        jobTitle: "NextJS Web Designed",
+        company: "NextJS Studio",
+        location: "Modesto, CA",
+        savedDate: "Saved Yesterday"
+    },
+    {
+        jobTitle: "NextJS Web Designed",
+        company: "NextJS Studio",
+        location: "Modesto, CA",
+        savedDate: "Saved Yesterday"
+    },
+    {
+        jobTitle: "NextJS Web Designed",
+        company: "NextJS Studio",
+        location: "Modesto, CA",
+        savedDate: "Saved Yesterday"
+    },
+    {
+        jobTitle: "NextJS Web Designed",
+        company: "NextJS Studio",
+        location: "Modesto, CA",
+        savedDate: "Saved Yesterday"
+    },
+    {
+        jobTitle: "NextJS Web Designed",
+        company: "NextJS Studio",
+        location: "Modesto, CA",
+        savedDate: "Saved Yesterday"
+    },
+]
+
+export const interviewJobsArr: Array<Job> = [
+
+]
+
+export const archivedJobsArr: Array<Job> = [
+    {
+        jobTitle: "HTML Web Developer",
+        company: "HTML Development Studios",
+        location: "Riverbank, CA",
+        savedDate: "Saved Last Week"
+    },
+]
