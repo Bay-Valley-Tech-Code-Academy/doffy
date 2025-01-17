b50c01746593d227f6f1b47deb033922dda72f10
CMBVT2023 <cristianomirandabvt2023@gmail.com>
2025-01-17 11:42:23 -0800

    Added component arguments to make the component reusable

--- src/app/saved-jobs/SavedJob.tsx
+++ src/app/saved-jobs/SavedJob.tsx
@@ -1,3 +1,4 @@
+import Image from 'next/image'
 import {Button} from '../../components/ui/button'
 import {
     DropdownMenu,
@@ -7,28 +8,37 @@ import {
     DropdownMenuSeparator,
     DropdownMenuTrigger,
   } from "../../components/ui/dropdown-menu"
+import PlaceHolderImage from './assets/PlaceHolderImage'
 
-export default function SavedJob({}) {
+export default function SavedJob({jobTitle, company, location, savedDate, companyImage}) {
     return (
-        <div>
-            <img />
-            <h3>Job title</h3>
-            <h4>Company</h4>
-            <h4>Location</h4>
-            <h4>Saved Date</h4>
-            <Button>Apply Now</Button>
-            <Button>Saved</Button>
-            <DropdownMenu>
-            <DropdownMenuTrigger>Open</DropdownMenuTrigger>
-                <DropdownMenuContent>
-                    <DropdownMenuLabel>Move To...</DropdownMenuLabel>
-                    <DropdownMenuSeparator />
-                    <DropdownMenuItem>Saved</DropdownMenuItem>
-                    <DropdownMenuItem>Applied</DropdownMenuItem>
-                    <DropdownMenuItem>Interviews</DropdownMenuItem>
-                    <DropdownMenuItem>Archived</DropdownMenuItem>
-                </DropdownMenuContent>
-            </DropdownMenu>
+        <div className='flex gap-2 p-2'>
+            <div className='p-2'>
+                {companyImage ? <Image src={companyImage} /> : <PlaceHolderImage height={25} width={25}/>}
+            </div>
+
+            <div className='flex flex-col'>
+                <h3 className='font-bold'>{jobTitle}</h3>
+                <h4 className='text-sm'>{company}</h4>
+                <h4 className='text-sm'>{location}</h4>
+                <h4 className='text-sm font-extralight'>{savedDate}</h4>
+            </div>
+
+            <div className='ms-auto flex content-center items-start gap-2 p-2'>
+                <Button>Apply Now</Button>
+                <Button className='text-extrabold text-xl text-white'>S</Button>
+                <DropdownMenu>
+                <DropdownMenuTrigger className='text-lg font-bold h-auto'>...</DropdownMenuTrigger>
+                    <DropdownMenuContent>
+                        <DropdownMenuLabel>Move To...</DropdownMenuLabel>
+                        <DropdownMenuSeparator />
+                        <DropdownMenuItem>Saved</DropdownMenuItem>
+                        <DropdownMenuItem>Applied</DropdownMenuItem>
+                        <DropdownMenuItem>Interviews</DropdownMenuItem>
+                        <DropdownMenuItem>Archived</DropdownMenuItem>
+                    </DropdownMenuContent>
+                </DropdownMenu>
+            </div>
         </div>
     )
 }
