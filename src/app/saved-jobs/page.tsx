c5e2ae8c82f961c14ce6f67180590c73ce5a2039
CMBVT2023 <cristianomirandabvt2023@gmail.com>
2025-01-26 18:58:51 -0800

    Fixed miscalculation on navbar and margin height

--- src/app/saved-jobs/page.tsx
+++ src/app/saved-jobs/page.tsx
@@ -5,7 +5,7 @@ export default function SavedJobsPage() {
 
   // Currently, I am unable to find a way to specify how to take the remainder of the screen up height wise without changing the main layout file.
   return (
-    <main className='flex justify-center h-[calc(100dvh-58px)] border-4'>
+    <main className='flex justify-center h-[calc(100dvh-60px)] border-4'>
       <div className="container w-full sm:w-3/4 md:w-3/5 h-full items-center justify-center gap-3 px-2 py-10">
           <h2 className="self-start text-4xl sm:text-5xl font-extrabold">
             Bookmarked Jobs
