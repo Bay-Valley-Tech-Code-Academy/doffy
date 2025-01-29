66cc84d1fba3fffff24e97235793f562068fbf35
CMBVT2023 <cristianomirandabvt2023@gmail.com>
2025-01-28 16:14:59 -0800

    Increased the navbar height in the calculation made when determining the height of the main container for the saved-jobs tab

--- src/app/saved-jobs/page.tsx
+++ src/app/saved-jobs/page.tsx
@@ -4,7 +4,7 @@ export default function SavedJobsPage() {
 
   // Currently, I am unable to find a way to specify how to take the remainder of the screen up height wise without changing the main layout file.
   return (
-    <main className='flex justify-center h-[calc(100dvh-60px)]'>
+    <main className='flex justify-center h-[calc(100dvh-65px)]'>
       <div className="container w-full sm:w-3/4 md:w-3/5 h-full items-center justify-center gap-3 px-2 py-10">
           <h2 className="self-start text-4xl sm:text-5xl font-extrabold">
             Bookmarked Jobs
