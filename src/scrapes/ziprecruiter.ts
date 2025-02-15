b538474c0b529aa9664b9aa2ee388105ec8a0beb
CMBVT2023 <cristianomirandabvt2023@gmail.com>
2025-02-14 16:29:57 -0800

    Created a ziprecruiter scrape to test out the puppeteer stealth plugin

---
+++ src/scrapes/ziprecruiter.ts
@@ -0,0 +1,58 @@
+import { chromium } from "playwright-extra";
+import StealthPlugin from "puppeteer-extra-plugin-stealth";
+
+// Enables playwright extra to utilize the stealth plugin.
+chromium.use(StealthPlugin());
+
+// Provides a user agent to the browser in multiple ways to appear less like a bot.
+const userAgent = 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/129.0.0.0 Safari/537.36';
+const userAgentHTTPHeaders = {
+  'Accept-Language': 'en-US,en;q=0.9',
+};
+
+const scrapeZipRecruiter = async () => {
+    const launchOptions = {
+            headless: true
+          }
+    
+    const mainBrowser = await chromium.launch(launchOptions);
+
+  try {
+        const mainContext = await mainBrowser.newContext({ 
+          userAgent,
+          viewport: { width: 1280, height: 720 }
+         });
+
+        await mainContext.addInitScript(
+          "Object.defineProperty(navigator, 'webdriver', { get: () => undefined })",
+        );
+    
+        const newPage = await mainContext.newPage();
+
+        await newPage.setExtraHTTPHeaders(userAgentHTTPHeaders);
+
+        await newPage.goto('https://www.ziprecruiter.com/jobs-search?form=jobs-landing&search=Programming&location=Oakdale%2C+CA&lvk=K3VL4e3ZonyLWph8RCo7tA.--NhxyzRG0J');
+
+        const jobInfo = await newPage.$$eval('div.job_result_two_pane.relative.h-full', (divs) => {
+          console.log('Found all dom elements.')
+          const data: Array<string | null> = [];
+          for (const div of divs) {
+            console.log('Iterated through a dom element')
+            data.push(`${div.textContent}\n`);
+          };
+          console.log('Returning data')
+          return data;
+        })
+
+        console.log(jobInfo);
+
+        await mainContext.close();
+        
+        await mainBrowser.close();
+    } catch (error) {
+        console.error(error);
+        await mainBrowser.close();
+  }
+}
+
+export { scrapeZipRecruiter }
