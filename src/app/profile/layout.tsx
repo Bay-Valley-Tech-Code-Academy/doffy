f536b05aa88b329e33eaac296c78285674f28081
CMBVT2023 <cristianomirandabvt2023@gmail.com>
2025-04-25 12:06:16 -0700

    Centered the main page display and started adding profile page sidebar options

--- src/app/profile/layout.tsx
+++ src/app/profile/layout.tsx
@@ -6,8 +6,8 @@ export default function ProfilePageLayout({ children }: { children: React.ReactN
     <main className="h-[calc(100dvh-65px)] w-full">
       <SidebarProvider className="h-full min-h-[calc(100dvh-65px)]" defaultOpen>
         <ProfilePageSideBar />
-        <div className="w-full">
-          <SidebarTrigger />
+        <div className="w-full h-full flex flex-col justify-center items-center">
+          <SidebarTrigger className='self-start'/>
           {children}
         </div>
       </SidebarProvider>
--- src/app/profile/page.tsx
+++ src/app/profile/page.tsx
@@ -5,5 +5,9 @@ import { SidebarProvider, SidebarTrigger } from '~/components/ui/sidebar';
 import ProfilePageSideBar from './components/profile-page-sidebar';
 
 export default function ProfilePage() {
-  return <h2>Temp</h2>;
+  return (
+    <div className="container h-full w-full items-center justify-center gap-3 px-2 py-5 sm:w-3/4 md:w-3/5">
+      <h2 className="self-start text-4xl font-extrabold sm:text-5xl">Information</h2>
+    </div>
+  );
 }
