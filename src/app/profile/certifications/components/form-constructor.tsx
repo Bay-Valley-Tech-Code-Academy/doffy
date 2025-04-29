65bc010af29d280e487fd7c04c64ecefee4791dd
CMBVT2023 <cristianomirandabvt2023@gmail.com>
2025-04-29 12:51:32 -0700

    Added a update button to trigger a useCallBack and rerender the form with the necessary inputs and formSchema

--- src/app/profile/certifications/components/form-constructor.tsx
+++ src/app/profile/certifications/components/form-constructor.tsx
@@ -1,23 +1,41 @@
 'use client';
-import { type ChangeEvent, useState } from 'react';
+import { type ChangeEvent, useCallback, useState } from 'react';
 import { Input } from '~/components/ui/input';
 import CertificationForm from '../../components/forms/certification-form';
+import { Button } from '~/components/ui/button';
 
 export default function FormConstructor() {
-  const [fileUploadNum, setFileUploadNum] = useState<number>(1);
+  const [newFileUploadNum, setNewFileUploadNum] = useState<number>(1);
+  const [currentFileUploadNum, setCurrentFileUploadNum] = useState<number>(1);
 
-    function handleChange(e: ChangeEvent<HTMLInputElement>) {
-        let newNumber = parseInt(e.target.value)
-        if (newNumber <= 0) newNumber = 1;
-        if (newNumber > 5) newNumber = 5;
-        setFileUploadNum(newNumber)
-    }
+  function handleChange(e: ChangeEvent<HTMLInputElement>) {
+    let newNumber = parseInt(e.target.value);
+    if (newNumber <= 0) newNumber = 1;
+    if (newNumber > 5) newNumber = 5;
+    setNewFileUploadNum(newNumber);
+  }
+
+  function handleClick() {
+    setCurrentFileUploadNum(newFileUploadNum);
+  }
+
+  const RenderForm = useCallback(
+    () => <CertificationForm fileInputs={currentFileUploadNum} />,
+    [currentFileUploadNum],
+  );
 
   return (
     <div>
-      <Input type="number" value={fileUploadNum} onChange={handleChange} min={1} max={5}/>
-        
-      <CertificationForm fileInputs={fileUploadNum} />
+      <Input
+        type="number"
+        value={newFileUploadNum}
+        onChange={handleChange}
+        min={1}
+        max={5}
+      />
+      <Button onClick={handleClick}>Update</Button>
+
+      <RenderForm />
     </div>
   );
 }
