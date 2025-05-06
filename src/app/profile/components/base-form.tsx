68752b0e2726deab6216e7efd5b63a13dce8bb58
CMBVT2023 <cristianomirandabvt2023@gmail.com>
2025-05-06 16:16:05 -0700

    Moved the form control map function into a seperate component

--- src/app/profile/components/base-form.tsx
+++ src/app/profile/components/base-form.tsx
@@ -15,6 +15,7 @@ import {
 import { Button } from '~/components/ui/button';
 import { Input } from '~/components/ui/input';
 import type { AllFormValues, FormControlObj } from '../utils/types';
+import { useMemo } from 'react';
 
 interface BaseFormProps {
   submitFunction: (values: AllFormValues) => void;
@@ -34,41 +35,44 @@ export default function BaseForm({
     defaultValues: defaultFormValues,
   });
 
+  const RenderFormControls = useMemo(() =>
+    formControls.map((formControl) => (
+      <FormField
+        control={form.control}
+        key={formControl.controlName}
+        name={formControl.controlName.toLowerCase()}
+        render={({ field }) => (
+          <FormItem>
+            <FormLabel>{formControl.controlName}</FormLabel>
+            <FormControl>
+              {formControl.controlType === 'file' ? (
+                <Input
+                  placeholder={formControl.controlPlaceHolder}
+                  type="file"
+                  accept={formControl.possibleFiles}
+                  onChange={(event) => {
+                    field.onChange(event.target.files?.[0]);
+                  }}
+                />
+              ) : (
+                <Input
+                  placeholder={formControl.controlPlaceHolder}
+                  type={formControl.controlType}
+                  {...field}
+                />
+              )}
+            </FormControl>
+            <FormDescription>{formControl.controlDescription}</FormDescription>
+            <FormMessage />
+          </FormItem>
+        )}
+      />
+    )), [formControls, form.control]);
+
   return (
     <Form {...form}>
       <form onSubmit={form.handleSubmit(submitFunction)} className="flex flex-col gap-5">
-        {formControls.map((formControl) => (
-          <FormField
-            control={form.control}
-            key={formControl.controlName}
-            name={formControl.controlName.toLowerCase()}
-            render={({ field }) => (
-              <FormItem>
-                <FormLabel>{formControl.controlName}</FormLabel>
-                <FormControl>
-                  {formControl.controlType === 'file' ? (
-                    <Input
-                      placeholder={formControl.controlPlaceHolder}
-                      type="file"
-                      accept={formControl.possibleFiles}
-                      onChange={(event) => {
-                        field.onChange(event.target.files?.[0]);
-                      }}
-                    />
-                  ) : (
-                    <Input
-                      placeholder={formControl.controlPlaceHolder}
-                      type={formControl.controlType}
-                      {...field}
-                    />
-                  )}
-                </FormControl>
-                <FormDescription>{formControl.controlDescription}</FormDescription>
-                <FormMessage />
-              </FormItem>
-            )}
-          />
-        ))}
+        {RenderFormControls}
 
         <Button type="submit">Save</Button>
       </form>
